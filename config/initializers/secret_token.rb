9935c50549dd1eae9e7fa19b675bff6aafd821b0
100lp <spb.gurme@gmail.com>
2012-11-17 17:36:27 +0400

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itart::Application.config.secret_token = '76edb81b16328238eb511470fcf6f7c7303767fbece73e9ad852a5f6f399841b9d8a20b5840e366844d0d7ee0c93ca4b2e6fe028c06d63123eaaa0ab0ce532ac'
